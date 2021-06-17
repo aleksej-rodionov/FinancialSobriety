f46a83ffabd3b77dd691e39bfa38c4ded8a84dc3
Aleksej <rodionovjr91@gmail.com>
2021-06-17 22:49:22 +0300

    i am drawing layout

---
+++ app/src/main/java/space/rodionov/financialsobriety/ui/MainActivity.kt
@@ -0,0 +1,36 @@
+package space.rodionov.financialsobriety.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import space.rodionov.financialsobriety.R
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController)
+
+    }
+
+
+
+}
+
+
+
+
